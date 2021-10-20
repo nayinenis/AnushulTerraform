9fd21d0bef1f831374d7b60a26a691fdb2df3784
snayineni devops <snayineni@gmail.com>
2021-10-20 10:15:20 -0400

    alld

--- TerraformBasica/Terraformvariablesdetailexplain/createInstance.tf
+++ TerraformBasica/Terraformvariablesdetailexplain/createInstance.tf
@@ -3,8 +3,9 @@ resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-090717c950a5c34d3"
   instance_type = "t2.micro"
   
-}
 
-tags = {
+
+  tags = {
     Name = "demoinstance"
+}  
 }
